aa6d0f0584e03d0caea7b22d9688bd237474ba5c
truongquangduy2002 <85172633+truongquangduy2002@users.noreply.github.com>
2021-11-28 20:31:26 +0700

    nhom10.4

--- detail.php
+++ detail.php
@@ -2,70 +2,389 @@
 include "header.php";
 ?>
 
+<!-- BREADCRUMB -->
+<div id="breadcrumb" class="section">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="breadcrumb-tree">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">All Categories</a></li>
+					<li><a href="#">Accessories</a></li>
+					<li><a href="#">Headphones</a></li>
+					<li class="active">Product name goes here</li>
+				</ul>
+			</div>
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!-- /BREADCRUMB -->
+
 <!-- SECTION -->
 <div class="section">
-    <!-- container -->
-    <div class="container">
-        <!-- row -->
-        <div class="row">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			<?php
+			if (isset($_GET['id'])) :
+				$id = $_GET['id'];
+				$detailProducts = $product->detailProduct($id);
+				foreach ($detailProducts as $value) :
 
-            <!-- STORE -->
-            <div id="store" class="col-md-14">
+			?>
+					<!-- Product main img -->
+					<div class="col-md-5 col-md-push-2">
+						<div id="product-main-img">
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
 
-                <!-- store products -->
-                <div class="row">
-                    <?php
-                    if(isset($_GET['id'])):
-                        $id = $_GET['id'];
-                        $getProductsById = $product->getProductsById($id);
-                        foreach ($getProductsById as $value) :
-                    ?>
-                            <!-- product -->
-                            <div class="col-md-6">
-                                <div class="product">
-                                    <div class="product-img">
-                                        <img src="./img/<?php echo $value['image'] ?>" alt="" width="100%" height="600">
-                                        <div class="product-label">
-                                            <span class="sale">-30%</span>
-                                            <span class="new">NEW</span>
-                                        </div>
-                                    </div>
-                                    <div class="product-body">
-                                        <div class="product-rating">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                        </div>
-                                    </div>
-                                   
-                                </div>
-                                
-                            </div>
-                            <div class="col-md-6">
-                            <h3 class="product-name"><?php echo $value['name'] ?></h3>
-                            <h4 class="product-price"><?php echo number_format($value['price']) ?>VND</h4>
-                            <h4 class="product-description ">Mô tả: <br> <?php echo $value['description'] ?></h4>
-                            <div class="add-to-cart">
-                                        <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-                                    </div>
-                            </div>
-                            <!-- /product -->
-                    <?php
-                        endforeach;
-                    endif
-                    ?>
-                </div>
-                <!-- /store products -->
-            </div>
-            <!-- /STORE -->
-        </div>
-        <!-- /row -->
-    </div>
-    <!-- /container -->
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+						</div>
+					</div>
+					<!-- /Product main img -->
+
+					<!-- Product thumb imgs -->
+					<div class="col-md-2  col-md-pull-5">
+						<div id="product-imgs">
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+
+							<div class="product-preview">
+								<img src="./img/<?php echo $value['image'] ?>" alt="">
+							</div>
+						</div>
+					</div>
+					<!-- /Product thumb imgs -->
+
+					<!-- Product details -->
+					<div class="col-md-5">
+						<div class="product-details">
+							<h2 class="product-name"><?php echo $value['name'] ?></h2>
+
+							<div>
+								<h3 class="product-price"><?php echo number_format($value['price']) ?> VND</h3>
+							</div>
+							<p><?php echo $value['description'] ?></p>
+							<div class="add-to-cart">
+								<div class="qty-label">
+									Qty
+									<div class="input-number">
+										<input type="number">
+										<span class="qty-up">+</span>
+										<span class="qty-down">-</span>
+									</div>
+								</div>
+								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+							</div>
+						</div>
+					</div>
+					<!-- /Product details -->
+
+
+
+					<!-- Product tab -->
+					<div class="col-md-12">
+						<div id="product-tab">
+							<!-- product tab nav -->
+							<ul class="tab-nav">
+								<li class="active"><a data-toggle="tab" href="#tab1">Description</a></li>
+								<li><a data-toggle="tab" href="#tab2">Details</a></li>
+								<li><a data-toggle="tab" href="#tab3">Reviews (3)</a></li>
+							</ul>
+							<!-- /product tab nav -->
+					<?php
+				endforeach;
+			endif;
+					?>
+					<!-- product tab content -->
+					<div class="tab-content">
+						<!-- tab1  -->
+						<div id="tab1" class="tab-pane fade in active">
+							<div class="row">
+								<div class="col-md-12">
+									<p><?php echo $value['description'] ?></p>
+								</div>
+							</div>
+						</div>
+						<!-- /tab1  -->
+
+						<!-- tab2  -->
+						<div id="tab2" class="tab-pane fade in">
+							<div class="row">
+								<div class="col-md-12">
+									<p><?php echo $value['description'] ?></p>
+								</div>
+							</div>
+						</div>
+						<!-- /tab2  -->
+
+						<!-- tab3  -->
+						<div id="tab3" class="tab-pane fade in">
+							<div class="row">
+								<!-- Rating -->
+								<div class="col-md-3">
+									<div id="rating">
+										<div class="rating-avg">
+											<span>4.5</span>
+											<div class="rating-stars">
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star"></i>
+												<i class="fa fa-star-o"></i>
+											</div>
+										</div>
+										<ul class="rating">
+											<li>
+												<div class="rating-stars">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="rating-progress">
+													<div style="width: 80%;"></div>
+												</div>
+												<span class="sum">3</span>
+											</li>
+											<li>
+												<div class="rating-stars">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star-o"></i>
+												</div>
+												<div class="rating-progress">
+													<div style="width: 60%;"></div>
+												</div>
+												<span class="sum">2</span>
+											</li>
+											<li>
+												<div class="rating-stars">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+												</div>
+												<div class="rating-progress">
+													<div></div>
+												</div>
+												<span class="sum">0</span>
+											</li>
+											<li>
+												<div class="rating-stars">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+												</div>
+												<div class="rating-progress">
+													<div></div>
+												</div>
+												<span class="sum">0</span>
+											</li>
+											<li>
+												<div class="rating-stars">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+													<i class="fa fa-star-o"></i>
+												</div>
+												<div class="rating-progress">
+													<div></div>
+												</div>
+												<span class="sum">0</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+								<!-- /Rating -->
+
+								<!-- Reviews -->
+								<div class="col-md-6">
+									<div id="reviews">
+										<ul class="reviews">
+											<li>
+												<div class="review-heading">
+													<h5 class="name">John</h5>
+													<p class="date">27 DEC 2018, 8:0 PM</p>
+													<div class="review-rating">
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star-o empty"></i>
+													</div>
+												</div>
+												<div class="review-body">
+													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+												</div>
+											</li>
+											<li>
+												<div class="review-heading">
+													<h5 class="name">John</h5>
+													<p class="date">27 DEC 2018, 8:0 PM</p>
+													<div class="review-rating">
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star-o empty"></i>
+													</div>
+												</div>
+												<div class="review-body">
+													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+												</div>
+											</li>
+											<li>
+												<div class="review-heading">
+													<h5 class="name">John</h5>
+													<p class="date">27 DEC 2018, 8:0 PM</p>
+													<div class="review-rating">
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star-o empty"></i>
+													</div>
+												</div>
+												<div class="review-body">
+													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+												</div>
+											</li>
+										</ul>
+										<ul class="reviews-pagination">
+											<li class="active">1</li>
+											<li><a href="#">2</a></li>
+											<li><a href="#">3</a></li>
+											<li><a href="#">4</a></li>
+											<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<!-- /Reviews -->
+
+								<!-- Review Form -->
+								<div class="col-md-3">
+									<div id="review-form">
+										<form class="review-form">
+											<input class="input" type="text" placeholder="Your Name">
+											<input class="input" type="email" placeholder="Your Email">
+											<textarea class="input" placeholder="Your Review"></textarea>
+											<div class="input-rating">
+												<span>Your Rating: </span>
+												<div class="stars">
+													<input id="star5" name="rating" value="5" type="radio"><label for="star5"></label>
+													<input id="star4" name="rating" value="4" type="radio"><label for="star4"></label>
+													<input id="star3" name="rating" value="3" type="radio"><label for="star3"></label>
+													<input id="star2" name="rating" value="2" type="radio"><label for="star2"></label>
+													<input id="star1" name="rating" value="1" type="radio"><label for="star1"></label>
+												</div>
+											</div>
+											<button class="primary-btn">Submit</button>
+										</form>
+									</div>
+								</div>
+								<!-- /Review Form -->
+							</div>
+						</div>
+						<!-- /tab3  -->
+					</div>
+					<!-- /product tab content  -->
+						</div>
+					</div>
+					<!-- /product tab -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
 </div>
 <!-- /SECTION -->
+
+<!-- Section -->
+<div class="section">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+
+			<div class="col-md-12">
+				<div class="section-title text-center">
+					<h3 class="title">Related Products</h3>
+				</div>
+			</div>
+			<?php
+				$getRelatedProducts = $product->getRelatedProducts();
+				foreach ($getRelatedProducts as $value) :
+
+			?>
+			<!-- product -->
+			<div class="col-md-3 col-xs-6">
+				<div class="product">
+					<div class="product-img">
+						<img src="./img/<?php echo $value['image'] ?>" alt="">
+						<div class="product-label">
+							<span class="sale">-30%</span>
+						</div>
+					</div>
+					<div class="product-body">
+						<p class="product-category">Category</p>
+						<h3 class="product-name"><a href="detail.php?id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h3>
+						<h4 class="product-price"><?php echo number_format($value['price']) ?> VND</h4>
+						<div class="product-rating">
+						</div>
+						<div class="product-btns">
+							<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+							<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+							<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+						</div>
+					</div>
+					<div class="add-to-cart">
+						<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+					</div>
+				</div>
+			</div>
+			<!-- /product -->
+			<?php
+				endforeach;
+			?>
+
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!-- /Section -->
 
 <?php
 include "footer.html";
